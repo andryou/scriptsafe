b27e4582557e1fc05fa4bc9235462bbee15be597
andryou <andryou@gmail.com>
2017-02-06 17:33:43 -0500

    Keypress delta option, panel icon updates

--- js/options.js
+++ js/options.js
@@ -345,6 +345,8 @@ function loadOptions() {
 	loadCheckbox("bluetooth");
 	loadElement("timezone");
 	loadCheckbox("keyboard");
+	if (!$("#keyboard").prop('checked')) $(".keydeltarow").hide();
+	loadElement("keydelta");
 	loadCheckbox("webbugs");
 	loadCheckbox("utm");
 	loadCheckbox("hashchecking");
@@ -413,6 +415,9 @@ function saveOptions() {
 	saveCheckbox("bluetooth");
 	saveElement("timezone");
 	saveCheckbox("keyboard");
+	if (!$("#keyboard").prop('checked')) $(".keydeltarow").hide();
+	else $(".keydeltarow").show();
+	saveElement("keydelta");
 	saveCheckbox("webbugs");
 	saveCheckbox("utm");
 	saveCheckbox("hashchecking");
