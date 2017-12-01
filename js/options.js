91332dcd51feedf293f1855f9d28c2647037078a
Andrew <andryou@gmail.com>
2017-12-01 08:29:37 -0500

    Bump to v1.0.9.4; will eventually sync all browser versioning soon.

--- js/options.js
+++ js/options.js
@@ -2,7 +2,7 @@
 // Distributed under the terms of the GNU General Public License
 // The GNU General Public License can be found in the gpl.txt file. Alternatively, see <http://www.gnu.org/licenses/>.
 'use strict';
-var version = '1.0.9.3';
+var version = '1.0.9.4';
 var bkg = chrome.extension.getBackgroundPage();
 var settingnames = [];
 var syncstatus;
